772c7feea21dac767381300822e2525584e393b2
rabbialamph <mdrabbialom98@gmail.com>
2026-03-30 09:53:31 +0600

    tailwind-daisyUi-lucideIcon install and components setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navber from './Components/Navber/Navber'
+import Banner from './Components/Banner/Banner'
+
+function App() {
+
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App
